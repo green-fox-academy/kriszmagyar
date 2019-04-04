28795c19df4234c999ff119f556af8426d157181
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 12:41:01 +0200

    add variable mutation

--- week-01/day-04/basics/src/Variables.java
+++ week-01/day-04/basics/src/Variables.java
@@ -38,5 +38,45 @@ public class Variables {
         System.out.println("Height: " + heightInMeter + "m");
         System.out.println("Married: " + isMarried);
 
+
+        // Variable mutation
+        int a = 3;
+        a += 10;
+        System.out.println("a: " + a);
+
+        int b = 100;
+        b -= 100;
+        System.out.println("b: " + b);
+
+        int c = 44;
+        c *= 2;
+        System.out.println("c: " + c);
+
+        int d = 125;
+        d /= 5;
+        System.out.println("d: " + d);
+
+        int e = 8;
+        e = e * e * e;
+        System.out.println("e: " + e);
+
+        int f1 = 123;
+        int f2 = 345;
+        System.out.println("f: " + (f1 > f2));
+
+        int g1 = 350;
+        int g2 = 200;
+        System.out.println("g: " + ((double) g2 > g1));
+
+        int h = 135798745;
+        System.out.println("h: " + (h % 11 == 0));
+
+        int i1 = 10;
+        int i2 = 3;
+        System.out.println("i: " + ((i1 > i2 * i2) && (i1 < i2 * i2 * i2)));
+
+        int j = 1521;
+        System.out.println("j: " + ((j % 3 == 0) || (j % 5 == 0)));
+
     }
 }
