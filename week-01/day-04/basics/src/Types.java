d7243a8a3a7fa877c28d35c0d08eee8181022a28
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 11:38:03 +0200

    add introduce yourself

---
+++ week-01/day-04/basics/src/Types.java
@@ -0,0 +1,10 @@
+public class Types {
+    public static void main(String[] args) {
+
+        // Introduce yourself
+        System.out.println("Krisz");
+        System.out.println(26);
+        System.out.println(1.85);
+        
+    }
+}
