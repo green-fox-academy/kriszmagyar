5bdc0d31bd5d75ff0c1ab9717973179483f030a4
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 15:25:20 +0200

    add fizzbuzz

--- week-01/day-04/basics/src/Loops.java
+++ week-01/day-04/basics/src/Loops.java
@@ -32,5 +32,18 @@ public class Loops {
             }
         }
 
+        // FizzBuzz
+        for (int i = 1; i < 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
     }
 }
