aa0587bb5da1f1e289fcc7f048be68ef43e306a3
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 12:16:41 +0200

    add define basic info

--- week-01/day-04/basics/src/Variables.java
+++ week-01/day-04/basics/src/Variables.java
@@ -5,6 +5,7 @@ public class Variables {
         int favoriteNumber = 23;
         System.out.println("\"My favorite number is: " + favoriteNumber + "\"");
 
+
         // Swap
         int a = 123;
         int b = 526;
@@ -17,12 +18,25 @@ public class Variables {
         System.out.println("a: " + a);
         System.out.println("b: " + b);
 
+
         // BMI
         double massInKg  = 81.2;
         double heightInM = 1.78;
 
         double bmi = massInKg / Math.pow(heightInM, 2);
         System.out.println("BMI: " + bmi);
+
+
+        // Define basic info
+        String  name          = "Krisz";
+        int     age           = 26;
+        double  heightInMeter = 1.85;
+        boolean isMarried     = false;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + heightInMeter + "m");
+        System.out.println("Married: " + isMarried);
 
     }
 }
