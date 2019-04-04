5dc64a55399025563b18aaae8af75b533705dd74
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 11:30:37 +0200

    add humpy dumpty

--- week-01/day-04/basics/src/PrintToTerminal.java
+++ week-01/day-04/basics/src/PrintToTerminal.java
@@ -4,5 +4,11 @@ public class PrintToTerminal {
         // Hello Me!
         System.out.println("Hello, Krisz!");
 
+        // Humpy Dumpty
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+
     }
 }
