6bcbc4c07a7bdca6cfccbdd6e445362f99363a36
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 11:40:49 +0200

    add two numbers

--- week-01/day-04/basics/src/Types.java
+++ week-01/day-04/basics/src/Types.java
@@ -5,6 +5,14 @@ public class Types {
         System.out.println("Krisz");
         System.out.println(26);
         System.out.println(1.85);
-        
+
+        // Two numbers
+        System.out.println(22 + 13);
+        System.out.println(22 - 13);
+        System.out.println(22 * 13);
+        System.out.println(22 / 13.);
+        System.out.println(22 / 13);
+        System.out.println(22 % 13);
+
     }
 }
