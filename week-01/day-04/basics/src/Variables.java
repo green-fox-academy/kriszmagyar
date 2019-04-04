892f7760063eb35f174f786a3e4b3b2d4d7d10bb
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 12:01:54 +0200

    add swap

--- week-01/day-04/basics/src/Variables.java
+++ week-01/day-04/basics/src/Variables.java
@@ -5,5 +5,17 @@ public class Variables {
         int favoriteNumber = 23;
         System.out.println("\"My favorite number is: " + favoriteNumber + "\"");
 
+        // Swap
+        int a = 123;
+        int b = 526;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
     }
 }
