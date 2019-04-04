9fd5fb4f238f52b035f304d6ae7fde27add1c141
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 12:59:42 +0200

    add seconds in a day

--- week-01/day-04/basics/src/Variables.java
+++ week-01/day-04/basics/src/Variables.java
@@ -90,5 +90,14 @@ public class Variables {
         System.out.println("Surface area: " + (int) surfaceArea);
         System.out.println("Volume: " + (int) volume);
 
+
+        // Seconds in a day
+        int currentHours   = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+
+        int remainingSeconds = 24 * 60 * 60 - (currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds);
+        System.out.println("Remaining seconds: " + remainingSeconds);
+
     }
 }
