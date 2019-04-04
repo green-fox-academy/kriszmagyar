daf4d4fe1cb6e74ee5d1f1a6b0166ef500f66ade
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 14:23:26 +0200

    add one two a lot

--- week-01/day-04/basics/src/Conditionals.java
+++ week-01/day-04/basics/src/Conditionals.java
@@ -14,5 +14,20 @@ public class Conditionals {
             System.out.println("Odd");
         }
 
+
+        // One two a lot
+        System.out.print("Give me a number: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Not enough");
+        } else if (number == 1) {
+            System.out.println("One");
+        } else if (number == 2) {
+            System.out.println("Two");
+        } else {
+            System.out.println("A lot");
+        }
+
     }
 }
