7444a7fe4aea9489c9a92d52a8c89d3bfa9db3dd
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 15:47:50 +0200

    add draw chess table

--- week-01/day-04/basics/src/Loops.java
+++ week-01/day-04/basics/src/Loops.java
@@ -89,5 +89,17 @@ public class Loops {
             }
         }
 
+        // Draw chess table
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("%");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
     }
 }
