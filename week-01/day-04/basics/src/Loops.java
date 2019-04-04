11814988560bf3816fc2bc6d50c4da8d18144297
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-04 15:01:56 +0200

    add i wont cheat on exams

---
+++ week-01/day-04/basics/src/Loops.java
@@ -0,0 +1,10 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        // I wont cheat on exams
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I won't cheat on exam!");
+        }
+
+    }
+}
