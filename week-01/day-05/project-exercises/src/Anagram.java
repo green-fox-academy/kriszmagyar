8f057010c731b24fc44dc51322b21a0132c792c1
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-05 09:59:53 +0200

    add anagram

---
+++ week-01/day-05/project-exercises/src/Anagram.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+
+        String a1 = "Dog";
+        String b1 = "god";
+        System.out.println(isAnagram(a1, b1)); // expects true
+
+        String a2 = "green";
+        String b2 = "fox";
+        System.out.println(isAnagram(a2, b2)); // expects false
+
+    }
+
+    // An anagram is direct word switch or word play,
+    // the result of rearranging the letters of a word or phrase
+    // to produce a new word or phrase, using all the original letters exactly once;
+    // for example, the word anagram can be rearranged into nag-a-ram.
+
+    private static boolean isAnagram(String a, String b) {
+
+        char[] arrFromA = a.toLowerCase().toCharArray();
+        char[] arrFromB = b.toLowerCase().toCharArray();
+
+        Arrays.sort(arrFromA);
+        Arrays.sort(arrFromB);
+
+        return Arrays.equals(arrFromA, arrFromB);
+    }
+
+}
