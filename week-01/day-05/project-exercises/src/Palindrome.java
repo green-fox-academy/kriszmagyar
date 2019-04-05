f3d53548cae6f3704604ad52b8924dc062faecb0
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-05 10:57:05 +0200

    add search palindrome

--- week-01/day-05/project-exercises/src/Palindrome.java
+++ week-01/day-05/project-exercises/src/Palindrome.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+
 public class Palindrome {
     public static void main(String[] args) {
+
+        // Test cases for createPalindrome
 
         String str1 = "";
         System.out.println(createPalindrome(str1)); // expects ""
@@ -12,6 +16,21 @@ public class Palindrome {
 
         String str4 = "racecar";
         System.out.println(createPalindrome(str4)); // expects "racecar"
+
+
+        // Test cases for search palindrome
+
+        String txt1 = "dog goat dad duck doodle never";
+        System.out.println(searchPalindrome(txt1)); // expects ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
+
+        String txt2 = "";
+        System.out.println(searchPalindrome(txt2)); // expects []
+
+        String txt3 = "racecar";
+        System.out.println(searchPalindrome(txt3)); // expects ["racecar", "aceca", "cec"]
+
+        String txt4 = "";
+        System.out.println(searchPalindrome(txt4)); // expects []
 
     }
 
@@ -31,6 +50,30 @@ public class Palindrome {
     private static boolean isPalindrome(String word) {
         String reverse = new StringBuilder(word).reverse().toString();
         return reverse.equals(word);
+    }
+
+    private static ArrayList<String> searchPalindrome(String txt) {
+
+        int minLength = 3;
+        int txtLength = txt.length();
+        ArrayList<String> palindromes = new ArrayList<>();
+
+        // Iterate over the length of words
+        for (int i = txtLength; i >= minLength; i--) {
+
+            // Set the starting position of the substrings
+            for (int j = 0; j <= txtLength - i; j++) {
+
+                String sub = txt.substring(j, i + j);
+
+                if (isPalindrome(sub)) {
+                    palindromes.add(sub);
+                }
+
+            }
+        }
+
+        return palindromes;
     }
 
 }
