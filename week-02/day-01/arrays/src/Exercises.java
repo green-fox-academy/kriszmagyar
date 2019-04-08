2c1a3587c24c2c36a2043ef6a2df7245ddcfc05e
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-08 12:01:25 +0200

    add third (arr exercices)

---
+++ week-02/day-01/arrays/src/Exercises.java
@@ -0,0 +1,14 @@
+public class Exercises {
+
+  public static void main(String[] args) {
+
+    int[] q = {4, 5, 6, 7};
+    printThird(q); // expects 6
+
+  }
+
+  public static void printThird(int[] arr) {
+    System.out.println(arr[2]);
+  }
+
+}
