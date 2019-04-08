f1a3357b31ae7390acab3877b67883b51342201a
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-08 11:42:01 +0200

    add doubling

---
+++ week-02/day-01/functions/src/Doubling.java
@@ -0,0 +1,12 @@
+public class Doubling {
+
+  public static void main(String[] args) {
+    int baseNum = 123;
+    System.out.println(doubling(baseNum)); // expects 1246
+  }
+
+  public static int doubling(int num) { 
+    return num * 2;
+  }
+
+}
