9621cba5ddf84f39295c6e83cffda03ddead06b0
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-08 11:47:02 +0200

    add comment

--- week-02/day-01/functions/src/Doubling.java
+++ week-02/day-01/functions/src/Doubling.java
@@ -2,10 +2,10 @@ public class Doubling {
 
   public static void main(String[] args) {
     int baseNum = 123;
-    System.out.println(doubling(baseNum)); // expects 1246
+    System.out.println(doubling(baseNum)); // expects 246
   }
 
-  public static int doubling(int num) { 
+  public static int doubling(int num) {
     return num * 2;
   }
 
