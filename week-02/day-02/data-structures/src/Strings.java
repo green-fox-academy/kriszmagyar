4644b4a315d46a3acd761c74b2982e50bf56b25e
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-09 12:26:39 +0200

    add url fixer

--- week-02/day-02/data-structures/src/Strings.java
+++ week-02/day-02/data-structures/src/Strings.java
@@ -7,8 +7,19 @@ public class Strings {
     example = example.replace("dishwasher", "galaxy");
     System.out.println(example); // expects In a  galaxy far far away
 
-    
+    // URL fixer
+    String url = "https//www.reddit.com/r/nevertellmethebots";
+    System.out.println(urlFixer(url)); // expects https://www.reddit.com/r/nevertellmetheodds
+  }
 
+  private static String urlFixer(String url) {
+    String nextUrl = url.replace("bots", "odds");
+    nextUrl = insertAt(nextUrl, ":", nextUrl.lastIndexOf("//"));
+    return nextUrl;
+  }
+
+  private static String insertAt(String str, String insertion, int index) {
+    return str.substring(0, index) + insertion + str.substring(index);
   }
 
 }
