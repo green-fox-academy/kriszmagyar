acc18fad5377b27077ccfaaab74d6237d2232f65
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-09 12:16:59 +0200

    add simple replace

---
+++ week-02/day-02/data-structures/src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+
+  public static void main(String[] args) {
+
+    // Simple replace
+    String example = "In a dishwasher far far away";
+    example = example.replace("dishwasher", "galaxy");
+    System.out.println(example); // expects In a  galaxy far far away
+
+    
+
+  }
+
+}
