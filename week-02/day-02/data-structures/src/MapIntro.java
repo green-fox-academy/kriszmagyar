6b80e542fc8921a104eb689bed99a8d8419a4e2f
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-09 15:05:33 +0200

    add map intro II

--- week-02/day-02/data-structures/src/MapIntro.java
+++ week-02/day-02/data-structures/src/MapIntro.java
@@ -27,6 +27,32 @@ public class MapIntro {
     System.out.println(map.containsKey(100));
     map.clear();
 
+    System.out.println("\n-------------\n");
+
+    Map<String, String> books = new HashMap<>();
+    books.put("978-1-60309-452-8",	"A Letter to Jo");
+    books.put("978-1-60309-459-7",	"Lupus");
+    books.put("978-1-60309-444-3",	"Red Panda and Moon Bear");
+    books.put("978-1-60309-461-0",	"The Lab");
+
+    print(books);
+
+    books.remove("978-1-60309-444-3");
+    books.values().remove("The Lab");
+    print(books);
+
+    books.put("978-1-60309-450-4",	"They Called Us Enemy");
+    books.put("978-1-60309-453-5",	"Why Did We Trust Him?");
+    System.out.println(books.containsKey("478-0-61159-424-8"));
+    System.out.println(books.get("978-1-60309-453-5"));
+
+  }
+
+  private static void print(Map<String, String> map) {
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      System.out.println(entry.getValue() + " (" + entry.getKey() + ")");
+    }
+    System.out.println("-------------");
   }
 
 }
