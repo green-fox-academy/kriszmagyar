5b5788a2641ad768e90d10bc8cdc254e4cd701c3
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-09 14:03:28 +0200

    add list intro I

---
+++ week-02/day-02/data-structures/src/ListIntro.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListIntro {
+
+  public static void main(String[] args) {
+
+    List<String> names = new ArrayList<>();
+    System.out.println(names.size());
+
+    names.add("William");
+    System.out.println(names.isEmpty());
+
+    names.add("John");
+    names.add("Amanda");
+    System.out.println(names.size());
+    System.out.println(names.get(2));
+
+    for (String name : names) {
+      System.out.println(name);
+    }
+
+    for (int i = 0; i < names.size(); i++) {
+      System.out.println((i + 1) + ". " + names.get(i));
+    }
+
+    names.remove(1);
+
+    for (int i = names.size() - 1; i >= 0; i--) {
+      System.out.println(names.get(i));
+    }
+
+    names.clear();
+    System.out.println(names);
+
+  }
+
+}
