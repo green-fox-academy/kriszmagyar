fcc698d5f2f7853bd3edcac379109a32c0cf02f8
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-10 11:45:35 +0200

    rename

--- week-02/day-03/drawing/src/DrawingSquares.java
+++ week-02/day-03/drawing/src/DrawingSquares.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FourRectangles{
+public class DrawingSquares {
   private static void mainDraw(Graphics graphics){
 
     // draw four different size and color rectangles.
