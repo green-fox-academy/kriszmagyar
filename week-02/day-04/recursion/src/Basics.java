a3f666f7209e8e0a9b22fc79805b7754085091b8
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-11 10:25:39 +0200

    add sum digits

--- week-02/day-04/recursion/src/Basics.java
+++ week-02/day-04/recursion/src/Basics.java
@@ -3,6 +3,7 @@ public class Basics {
   public static void main(String[] args) {
 
     System.out.println(numberAdder(5)); // expects 15
+    System.out.println(sumDigits(135)); // expects 9
 
   }
 
@@ -10,5 +11,12 @@ public class Basics {
     return num == 1 ? 1 : num + numberAdder(num - 1);
   }
 
+  private static int sumDigits(int num) {
+    if (num / 10 == 0) {
+      return num;
+    } else {
+      return num % 10 + sumDigits(num / 10);
+    }
+  }
 
 }
