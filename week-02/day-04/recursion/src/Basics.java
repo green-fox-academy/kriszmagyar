80e068678b9058929d30a1c29324d82614d82b66
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-11 12:00:23 +0200

    add fibonacci

--- week-02/day-04/recursion/src/Basics.java
+++ week-02/day-04/recursion/src/Basics.java
@@ -11,6 +11,7 @@ public class Basics {
     System.out.println(replaceAll("my exes are xors", 'x', 'y')); // expects "my eyes are yors"
     System.out.println(removeAll("axle exye", 'x')); // expects "ale eye"
     System.out.println(addToAllChars("Hello there!", '*')); // expects "H*e*l*l*o* *t*h*e*r*e*!"
+    System.out.println(fibonacci(7)); // expects 13
 
   }
 
@@ -127,6 +128,25 @@ public class Basics {
    */
   private static String insertCharAt(String str, char c, int i) {
     return str.substring(0, i) + c + str.substring(i);
+  }
+
+  /**
+   * The fibonacci sequence is a famous bit of mathematics, and it happens to
+   * have a recursive definition. The first two values in the sequence are 0 and 1
+   * (essentially 2 base cases). Each subsequent value is the sum of the previous
+   * two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on.
+   * <br><br>
+   * Define a recursive fibonacci(n) method that returns the nth
+   * fibonacci number, with n=0 representing the start of the sequence.
+   */
+  private static int fibonacci(int num) {
+    if (num == 0) {
+      return 0;
+    }
+    if (num == 1) {
+      return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
   }
 
 }
