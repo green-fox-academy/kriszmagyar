30017856c878f1f6871b294462eeb3999c73095f
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-12 09:03:50 +0200

    refactor palindromes

--- week-01/day-05/project-exercises/src/Palindrome.java
+++ week-01/day-05/project-exercises/src/Palindrome.java
@@ -4,33 +4,16 @@ public class Palindrome {
     public static void main(String[] args) {
 
         // Test cases for createPalindrome
-
-        String str1 = "";
-        System.out.println(createPalindrome(str1)); // expects ""
-
-        String str2 = "greenfox";
-        System.out.println(createPalindrome(str2)); // expects "greenfoxxofneerg"
-
-        String str3 = "123";
-        System.out.println(createPalindrome(str3)); // expects "123321"
-
-        String str4 = "racecar";
-        System.out.println(createPalindrome(str4)); // expects "racecar"
-
+        System.out.println(createPalindrome(""));          // expects ""
+        System.out.println(createPalindrome("greenfox"));  // expects "greenfoxxofneerg"
+        System.out.println(createPalindrome("123"));       // expects "123321"
+        System.out.println(createPalindrome("racecar"));   // expects "racecar"
 
         // Test cases for search palindrome
-
-        String txt1 = "dog goat dad duck doodle never";
-        System.out.println(searchPalindrome(txt1)); // expects ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
-
-        String txt2 = "";
-        System.out.println(searchPalindrome(txt2)); // expects []
-
-        String txt3 = "racecar";
-        System.out.println(searchPalindrome(txt3)); // expects ["racecar", "aceca", "cec"]
-
-        String txt4 = "";
-        System.out.println(searchPalindrome(txt4)); // expects []
+        System.out.println(searchPalindrome("dog goat dad duck doodle never"));  // expects ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
+        System.out.println(searchPalindrome(""));                                // expects []
+        System.out.println(searchPalindrome("racecar"));                         // expects ["racecar", "aceca", "cec"]
+        System.out.println(searchPalindrome(""));                                // expects []
 
     }
 
@@ -38,41 +21,37 @@ public class Palindrome {
     // which reads the same backward as forward, such as madam or racecar.
 
     private static String createPalindrome(String word) {
-
         if (isPalindrome(word)) {
             return word;
         }
-
-        String reverse = new StringBuilder(word).reverse().toString();
-        return word.concat(reverse);
+        return word.concat(reverse(word));
     }
 
     private static boolean isPalindrome(String word) {
-        String reverse = new StringBuilder(word).reverse().toString();
-        return reverse.equals(word);
+        return word.equals(reverse(word));
+    }
+
+    private static String reverse(String word) {
+        return new StringBuilder(word).reverse().toString();
     }
 
     private static ArrayList<String> searchPalindrome(String txt) {
 
         int minLength = 3;
-        int txtLength = txt.length();
         ArrayList<String> palindromes = new ArrayList<>();
 
         // Iterate over the length of words
-        for (int i = txtLength; i >= minLength; i--) {
+        for (int i = txt.length(); i >= minLength; i--) {
 
             // Set the starting position of the substrings
-            for (int j = 0; j <= txtLength - i; j++) {
+            for (int j = 0; j <= txt.length() - i; j++) {
 
                 String sub = txt.substring(j, i + j);
-
                 if (isPalindrome(sub)) {
                     palindromes.add(sub);
                 }
-
             }
         }
-
         return palindromes;
     }
 
