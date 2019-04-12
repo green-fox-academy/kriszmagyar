2d31e1f974c17bfe401d19dd59c745854bf95fbd
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-12 12:15:51 +0200

    complete triangles

--- week-02/day-05/drawing-project/src/Triangles.java
+++ week-02/day-05/drawing-project/src/Triangles.java
@@ -8,16 +8,32 @@ public class Triangles {
     int size = 32;
     int numbOfLines = 26;
 
-    drawLines(graphics, size, numbOfLines);
+    drawTriangles(graphics, size, numbOfLines);
   }
 
-  private static void drawLines(Graphics graphics, int size, int n) {
+  private static void drawTriangles(Graphics graphics, int size, int n) {
 
     for (int i = 0; i < n; i++) {
       int x1 = i * size / 2;
       int y1 = (int) (HEIGHT - i * size / Math.sqrt(2));
       int x2 = WIDTH - i * size / 2;
       int y2 = y1;
+      graphics.drawLine(x1, y1, x2, y2);
+    }
+
+    for (int i = 0; i < n; i++) {
+      int x1 = i * size;
+      int y1 = HEIGHT;
+      int x2 = WIDTH / 2 + i * size / 2;
+      int y2 = HEIGHT - (int) ((HEIGHT - i * size) / Math.sqrt(2));
+      graphics.drawLine(x1, y1, x2, y2);
+    }
+
+    for (int i = 0; i < n; i++) {
+      int x1 = WIDTH - i * size;
+      int y1 = HEIGHT;
+      int x2 = WIDTH / 2 - i * size / 2;
+      int y2 = HEIGHT - (int) ((HEIGHT - i * size) / Math.sqrt(2));
       graphics.drawLine(x1, y1, x2, y2);
     }
 
