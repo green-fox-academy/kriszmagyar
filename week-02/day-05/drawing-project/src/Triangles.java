cd5af5bfade0205988df79d763d1900547cfe8fe
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-12 09:58:33 +0200

    add rotation to draw regular polygon

---
+++ week-02/day-05/drawing-project/src/Triangles.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Triangles {
+  private static void draw(Graphics graphics){
+
+    int x = WIDTH / 2;
+    int y = HEIGHT / 2;
+    int size = 256;
+
+    drawTriangle(graphics, x, y, size);
+
+  }
+
+  private static void drawTriangle(Graphics graphics, int x, int y, int size) {
+    drawRegularPolygon(graphics, x, y, size, 3, 60);
+  }
+
+  /**
+   * Draw a regular polygon with a given parameters.
+   *
+   * @param graphics The Graphics object which takes care about the actual drawing.
+   * @param x The x coordinate of the center of the polygon.
+   * @param y The y coordinate of the center of the polygon.
+   * @param size The length of the side.
+   * @param n The number of sides.
+   */
+  private static void drawRegularPolygon(Graphics graphics, int x, int y, int size, int n, int rotation) {
+
+    int[] xPoints = new int[n];
+    int[] yPoints = new int[n];
+
+    for (int i = 0; i < n; i++) {
+
+      double rotate = Math.PI * rotation / 360;
+      int xDiff = (int) (Math.cos(2 * Math.PI / n * i + rotate) * size);
+      int yDiff = (int) (Math.sin(2 * Math.PI / n * i + rotate) * size);
+
+      xPoints[i] = x + xDiff;
+      yPoints[i] = y + yDiff;
+
+    }
+
+    graphics.drawPolygon(xPoints, yPoints, n);
+  }
+
+  // Don't touch the code below
+  private static final int WIDTH = 730;
+  private static final int HEIGHT = 730;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Draw Squares");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      draw(graphics);
+    }
+  }
+}
