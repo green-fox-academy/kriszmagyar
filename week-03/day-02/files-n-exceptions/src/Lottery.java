ef1baf67931e8a94d18ec28d3e07645a57302632
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-16 16:28:32 +0200

    refactor(Lottery): add toIntArr method

--- week-03/day-02/files-n-exceptions/src/Lottery.java
+++ week-03/day-02/files-n-exceptions/src/Lottery.java
@@ -13,15 +13,6 @@ public class Lottery {
     System.out.println(mostCommon(data, 5));
   }
 
-  private static List<String> getData(String path) {
-    try {
-      return Files.readAllLines(Paths.get(path));
-    } catch (IOException e) {
-      e.printStackTrace();
-      return new ArrayList<>();
-    }
-  }
-
   private static List<Integer> mostCommon(List<String> data, int n) {
 
     List<int[]> winningNumbers = getWinningNumbers(data);
@@ -31,18 +22,33 @@ public class Lottery {
     return new ArrayList<>();
   }
 
+  private static List<String> getData(String path) {
+    try {
+      return Files.readAllLines(Paths.get(path));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return new ArrayList<>();
+    }
+  }
+
   private static List<int[]> getWinningNumbers(List<String> data) {
     List<int[]> results = new ArrayList<>();
+    int numberOfWinners = 5;
+
     for (String line : data) {
       String[] lineData = line.split(";");
-      String[] numbers = Arrays.copyOfRange(lineData, lineData.length - 5, lineData.length);
-      int[] wNumbers = new int[5];
-      for (int i = 0; i < numbers.length; i++) {
-        wNumbers[i] = Integer.parseInt(numbers[i]);
-      }
-      results.add(wNumbers);
+      String[] numbers = Arrays.copyOfRange(lineData, lineData.length - numberOfWinners, lineData.length);
+      results.add(toIntArr(numbers));
     }
     return results;
+  }
+
+  private static int[] toIntArr(String[] arr) {
+    int[] intArr = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      intArr[i] = Integer.parseInt(arr[i]);
+    }
+    return intArr;
   }
 
   private static void print(List<int[]> winningNumbers) {
