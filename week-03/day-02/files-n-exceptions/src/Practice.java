7c7728ed1b8bbeeece0411ac44646a12eaa4a42b
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-16 11:18:25 +0200

    feat(Practice): add print each line method

--- week-03/day-02/files-n-exceptions/src/Practice.java
+++ week-03/day-02/files-n-exceptions/src/Practice.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Practice {
 
   public static void main(String[] args) {
 
     divide(0); // excepts fail
+    printFromFile("assets/my-file.txt");
 
   }
 
@@ -11,6 +17,17 @@ public class Practice {
       System.out.println(10 / n);
     } catch (ArithmeticException e) {
       System.out.println("fail");
+    }
+  }
+
+  private static void printFromFile(String path) {
+    try {
+      List <String> lines =  Files.readAllLines(Paths.get(path));
+      for (String line : lines) {
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      System.out.println("Unable to read file: " + path);
     }
   }
 
