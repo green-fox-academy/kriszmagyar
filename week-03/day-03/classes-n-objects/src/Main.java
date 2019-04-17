4f027fad96426f858c807a27eb0b8ae5cf30d5db
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-17 11:28:32 +0200

    feat(PostIt): add PostIt class

--- week-03/day-03/classes-n-objects/src/Main.java
+++ week-03/day-03/classes-n-objects/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
   public static void main(String[] args) {
-    
+
+    PostIt idea = new PostIt("orange", "Idea 1", "blue");
+    PostIt awesome = new PostIt("pink", "Awesome", "black");
+    PostIt superb = new PostIt("yellow", "Superb", "green");
+
   }
 
 }
