09db41db63ce86052c0761d21e996fcdca292283
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-17 11:36:59 +0200

    feat(BlogPost): add BlogPost class

--- week-03/day-03/classes-n-objects/src/Main.java
+++ week-03/day-03/classes-n-objects/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class Main {
 
   public static void main(String[] args) {
@@ -5,6 +7,10 @@ public class Main {
     PostIt idea = new PostIt("orange", "Idea 1", "blue");
     PostIt awesome = new PostIt("pink", "Awesome", "black");
     PostIt superb = new PostIt("yellow", "Superb", "green");
+
+    BlogPost post1 = new BlogPost("John Doe", "Lorem Ipsum", "Lorem ipsum dolor sit amet.", "2000.05.04");
+    BlogPost post2 = new BlogPost("Tim Urban", "Wait but why", "A popular long-form, stick-figure-illustrated blog about almost everything.", "2010.10.10");
+    BlogPost post3 = new BlogPost("William Turton", "One Engineer Is Trying to Get IBM to Reckon With Trump", "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.", "2017.03.28");
 
   }
 
