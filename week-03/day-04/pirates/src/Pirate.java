85264e8a664a504647591db21338eecb24c4e4c3
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-18 10:02:36 +0200

    feat(Pirates): init Pirate class

---
+++ week-03/day-04/pirates/src/Pirate.java
@@ -0,0 +1,61 @@
+public class Pirate {
+
+  private static final int MAX_SOBER_LEVEL = 4;
+
+  private int drunkLevel = 0;
+  private boolean isPassedOut = false;
+  private boolean isDead = false;
+
+  void drinkSomeRum() {
+    if (this.isDead) {
+      System.out.println("He's dead.");
+      return;
+    }
+
+    System.out.println("Pirate is drinking rum.");
+    this.drunkLevel++;
+  }
+
+  void howsItGoingMate() {
+    if (isDrunk()) {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      this.passOut();
+    } else {
+      System.out.println("Pour me anudder! I just had " + this.drunkLevel + " drinks.");
+    }
+  }
+
+  void brawl(Pirate anotherPirate) {
+    if (this.isDead || anotherPirate.isDead) {
+      System.out.println("Dead pirates can't fight!");
+      return;
+    }
+
+    int rnd = (int) (Math.random() * 3);
+    if (rnd == 0) {
+      this.passOut();
+      anotherPirate.passOut();
+    }
+    if (rnd == 1) {
+      this.die();
+    }
+    if (rnd == 2) {
+      anotherPirate.die();
+    }
+  }
+
+  void die() {
+    System.out.println("This pirate died.");
+    this.isDead = true;
+  }
+
+  void passOut() {
+    System.out.println("This pirate just passed out.");
+    this.isPassedOut = true;
+  }
+
+  private boolean isDrunk() {
+    return this.drunkLevel > MAX_SOBER_LEVEL;
+  }
+
+}
