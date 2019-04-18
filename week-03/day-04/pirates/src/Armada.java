0ecfb0d48837dcba06862c440441736b693f595e
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-18 11:40:14 +0200

    feat(Pirates): init Armada

---
+++ week-03/day-04/pirates/src/Armada.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Armada {
+
+  private static final int MIN_FLEET = 4;
+  private static final int MAX_FLEET = 10;
+
+  private List<Ship> fleet;
+
+  public Armada() {
+    this.fleet = initFleet();
+  }
+
+  private List<Ship> initFleet() {
+    List<Ship> fleet = new ArrayList<>();
+    int numbOfShips = (int) (Math.random() * (MAX_FLEET - MIN_FLEET + 1) + MIN_FLEET);
+
+    for (int i = 0; i < numbOfShips; i++) {
+      fleet.add(new Ship());
+    }
+
+    return fleet;
+  }
+}
