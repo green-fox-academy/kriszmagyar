73a5f8239fcc39a4065da43f5efc31107155a6e2
Krisz Magyar <krisz.magyar@gmail.com>
2019-04-23 14:13:24 +0200

    feat(airforce): init Aircraft

---
+++ week-04/day-02/inheritance/src/airforce/Aircraft.java
@@ -0,0 +1,40 @@
+package airforce;
+
+public abstract class Aircraft {
+
+  private int maxAmmo;
+  private int currentAmmo;
+  private int baseDamage;
+
+  Aircraft() {
+    this.currentAmmo = 0;
+  }
+
+  int fight() {
+    int damage = getDamage();
+    this.currentAmmo = 0;
+    return damage;
+  }
+
+  int refill(int amount) {
+    int currentAmmo = this.currentAmmo;
+    this.currentAmmo = Math.min(this.currentAmmo + amount, this.maxAmmo);
+    return amount - this.currentAmmo + currentAmmo;
+  }
+
+  String getType() {
+    return this.getClass().getSimpleName();
+  }
+
+  String getStatus() {
+    return "Type " + getType() + ", Ammo: " + this.currentAmmo + ", Base Damage: "
+        + this.baseDamage + ", All damage: " + getDamage();
+  }
+
+  private int getDamage() {
+    return this.currentAmmo * this.baseDamage;
+  }
+
+  abstract boolean isPriority();
+
+}
