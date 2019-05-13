9706e2fc24a2cb901a2b289f16ce022c554ea2ea
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-13 14:25:03 +0200

    feat(GetIndex): init test case

---
+++ week-05/day-01/tdd-pair/src/getindex/GetIndex.java
@@ -0,0 +1,7 @@
+package getindex;
+
+public class GetIndex {
+
+
+
+}
---
+++ week-05/day-01/tdd-pair/test/getindex/GetIndexTest.java
@@ -0,0 +1,27 @@
+package getindex;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GetIndexTest {
+
+  private GetIndex getIndex;
+  private List<Integer> list;
+
+  @Before
+  public void setup() {
+    getIndex = new GetIndex();
+    list = new ArrayList<>(Arrays.asList(2, 5, 3, 7, 1));
+  }
+
+  @Test
+  public void getIndexShouldReturnOne() {
+    assertEquals(1, getIndex.get(list, 5));
+  }
+
+}
