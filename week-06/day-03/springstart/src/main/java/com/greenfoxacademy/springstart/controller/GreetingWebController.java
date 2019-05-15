62456b2134d9a558c269a4693e91a13ff8af51c4
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-15 12:47:15 +0200

    feat(Greet): add basic multi language hello solution

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingWebController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingWebController.java
@@ -1,8 +1,11 @@
 package com.greenfoxacademy.springstart.controller;
 
 import com.greenfoxacademy.springstart.components.Counter;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,6 +13,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingWebController {
 
   private Counter c = new Counter();
+  private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+      "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+      "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+      "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
   @RequestMapping("/web/greeting")
   public String greeting(Model model, @RequestParam String name) {
@@ -17,6 +24,16 @@ public class GreetingWebController {
     model.addAttribute("name", name);
     model.addAttribute("count", c.get());
     return "greeting";
+  }
+
+  @RequestMapping("/web/greetings")
+  public String greetings() {
+    return "greetings";
+  }
+
+  @ModelAttribute("hellos")
+  public List<String> hellos() {
+    return Arrays.asList(hellos);
   }
 
 }
