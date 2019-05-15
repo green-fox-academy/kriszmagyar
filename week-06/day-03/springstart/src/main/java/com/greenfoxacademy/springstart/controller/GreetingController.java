25237629f27d35baaad72eb175cfc031a4bb4a76
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-15 11:55:24 +0200

    feat(Greet Counter): add counter

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springstart.controller;
 
+import com.greenfoxacademy.springstart.components.Counter;
 import com.greenfoxacademy.springstart.components.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+  private Counter c = new Counter();
+
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam String name) {
-      return new Greeting(1, "Hello, " + name + "!");
+      c.inc();
+      return new Greeting(c.get(), "Hello, " + name + "!");
   }
 
 }
