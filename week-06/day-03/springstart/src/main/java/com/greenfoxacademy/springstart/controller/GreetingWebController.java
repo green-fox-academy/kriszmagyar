03084a186e7fd1a6d8b42972dcf4028ec8e609ca
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-15 12:17:28 +0200

    feat(Web Counter): add solution

--- week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingWebController.java
+++ week-06/day-03/springstart/src/main/java/com/greenfoxacademy/springstart/controller/GreetingWebController.java
@@ -1,15 +1,21 @@
 package com.greenfoxacademy.springstart.controller;
 
+import com.greenfoxacademy.springstart.components.Counter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingWebController {
 
+  private Counter c = new Counter();
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam String name) {
+    c.inc();
+    model.addAttribute("name", name);
+    model.addAttribute("count", c.get());
     return "greeting";
   }
 
