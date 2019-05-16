bb68ce0e3d3ac0cfd34d1e72374f5966a1ebb0ee
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-16 14:23:56 +0200

    feat(WebShopController): add only avaiable route

--- week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/controller/WebShopController.java
+++ week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/controller/WebShopController.java
@@ -2,9 +2,9 @@ package com.greenfoxacademy.webshop.controller;
 
 import com.greenfoxacademy.webshop.components.ShopItem;
 import com.greenfoxacademy.webshop.containers.ShoppingList;
-import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,13 +13,19 @@ public class WebShopController {
   private ShoppingList shoppingList = new ShoppingList();
 
   @RequestMapping ("/webshop")
-  public String getWebShop() {
+  public String getWebShop(Model model) {
+    model.addAttribute("shoppingList", shoppingList.get());
     return "webshop";
   }
 
-  @ModelAttribute ("shoppingList")
-  public List<ShopItem> shoppingList() {
-    return shoppingList.get();
+  @RequestMapping("/webshop/only-available")
+  public String getOnlyAvailable(Model model) {
+    model.addAttribute("shoppingList",
+        shoppingList.get().stream()
+        .filter(ShopItem::isAvaiable)
+        .collect(Collectors.toList())
+    );
+    return "webshop";
   }
 
 }
