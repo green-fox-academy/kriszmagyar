c1fb33dd723cbfefb9bbe57bdf08878ba5b73556
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-16 12:32:08 +0200

    feat(WebShopController): pass shoppingList to template

--- week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/controller/WebShopController.java
+++ week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/controller/WebShopController.java
@@ -1,14 +1,25 @@
 package com.greenfoxacademy.webshop.controller;
 
+import com.greenfoxacademy.webshop.components.ShopItem;
+import com.greenfoxacademy.webshop.containers.ShoppingList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebShopController {
 
-  @RequestMapping
+  private ShoppingList shoppingList = new ShoppingList();
+
+  @RequestMapping ("/webshop")
   public String getWebShop() {
     return "webshop";
+  }
+
+  @ModelAttribute ("shoppingList")
+  public List<ShopItem> shoppingList() {
+    return shoppingList.get();
   }
 
 }
