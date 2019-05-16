00052492e4a093b1b0cf57fbdf5171865d86855b
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-16 14:13:35 +0200

    feat(ShoppingList): populate with data

--- week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/containers/ShoppingList.java
+++ week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/containers/ShoppingList.java
@@ -15,7 +15,11 @@ public class ShoppingList {
 
   private void populate() {
     shoppingList = Arrays.asList(
-        new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000, 5)
+        new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000, 5),
+        new ShopItem("Printer", "Some HP printer that prints pages", 3000, 2),
+        new ShopItem("Coca cola", "0.5l standard coke", 25, 0),
+        new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119, 100),
+        new ShopItem("T-shirt", "Blue with a corgi on a bike", 1000, 5)
     );
   }
 
