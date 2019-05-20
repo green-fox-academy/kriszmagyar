a0c1b79f3c33d96093b05285d835f58547248039
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-20 14:09:07 +0200

    feat(view): add fragments

--- week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
+++ week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
@@ -1,6 +1,8 @@
 package com.greenfoxacademy.lionkingbank.controller;
 
 import com.greenfoxacademy.lionkingbank.model.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/accounts")
 public class AccountController {
 
+  List<BankAccount> accounts = new ArrayList<>();
+
+  @GetMapping("/")
+  public String getAccounts(Model model) {
+    model.addAttribute(accounts);
+    return "accounts";
+  }
+
   @GetMapping("/show")
   public String getAccount(Model model) {
     model.addAttribute("account",
-        new BankAccount("Simba", 2000.00, "lion"));
+        new BankAccount("Simba", 2000.0, "lion"));
     return "account";
   }
 
