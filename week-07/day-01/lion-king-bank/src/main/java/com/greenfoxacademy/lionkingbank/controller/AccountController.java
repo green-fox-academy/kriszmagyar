147fe9d2d2096b93e1af8b593ac92748c4f5a470
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-20 12:31:49 +0200

    feat(view): format currency

--- week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
+++ week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/accounts")
 public class AccountController {
 
-  @GetMapping("/accounts/show")
+  @GetMapping("/show")
   public String getAccount(Model model) {
     model.addAttribute("account",
-        new BankAccount("Simba", 2000.0, "lion"));
+        new BankAccount("Simba", 2000.00, "lion"));
     return "account";
   }
 
