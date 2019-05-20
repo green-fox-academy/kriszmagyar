62944854f02ca89be7463c073c073d1ee61e2db2
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-20 11:50:17 +0200

    feat(lk-bank): init

--- week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/LionKingBankApplication.java
+++ week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/LionKingBankApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LionKingBankApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LionKingBankApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LionKingBankApplication.class, args);
+  }
 
 }
