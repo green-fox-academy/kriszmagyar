ab303c58b6f772a08006e10de43b11fd25a4e67a
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-20 14:28:13 +0200

    feat(controller): add account with id

--- week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
+++ week-07/day-01/lion-king-bank/src/main/java/com/greenfoxacademy/lionkingbank/controller/AccountController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,9 +29,10 @@ public class AccountController {
     return "accounts";
   }
 
-  @GetMapping("/show")
-  public String getAccount(Model model) {
-    model.addAttribute("account", accounts.add(new BankAccount("Simba", 2000.0, "lion")));
+  @GetMapping("/{id}")
+  public String getAccount(Model model, @PathVariable int id) {
+    model.addAttribute("account",
+        accounts.stream().filter(a -> a.getId() == id).findAny().orElse(null));
     return "account";
   }
 
