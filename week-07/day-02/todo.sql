28551d8afe0f89c29dbe10a50fb47f1fc597048a
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-21 14:20:24 +0200

    feat(todoDB): add sql scripts

--- week-07/day-02/todo.sql
+++ week-07/day-02/todo.sql
@@ -20,19 +20,48 @@ show tables;
 describe users;
 describe tasks;
 
+insert into users
+	(name) values
+    ('Krisz'), ('Zsoti'), ('Geri'), ('Mate');
 
+select * from users;
 
+insert into tasks
+	(title, user_id) values
+    ('Walk the dog', 1),
+    ('Write homework', 1),
+    ('Copy homework of Krisz', 4),
+    ('Praise the gods', 4),
+    ('Be cool', 2),
+    ('Walk the dog again', 1);
 
+select * from tasks;
 
+update tasks
+set title = 'Copy the homework of Krisz'
+where title like 'Copy homework%';
 
+update tasks
+set is_completed = 1
+where user_id = 4
+and title like '%homework%'
+limit 1;
 
+delete from tasks
+where id = 6;
 
+select name, title from users join tasks
+on users.id = tasks.user_id
+where users.name = 'Krisz'
+order by tasks.title
+limit 1;
 
-
-
-
-
-
+select name, count(tasks.id) as NumbOfTasks from users left join tasks
+on users.id = tasks.user_id
+where tasks.is_completed = 0
+group by users.name
+having name in ('Krisz', 'Mate')
+order by NumbOfTasks asc;
 
 
 
