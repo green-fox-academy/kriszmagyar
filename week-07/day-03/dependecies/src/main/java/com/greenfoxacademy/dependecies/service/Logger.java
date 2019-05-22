beadf3c1c425cb28be0d77ca1668050ee912fc29
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-22 12:09:13 +0200

    feat(app): impl CommandLineRunner

---
+++ week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/service/Logger.java
@@ -0,0 +1,5 @@
+package com.greenfoxacademy.dependecies.service;
+
+public interface Logger {
+  void log(String msg);
+}
--- week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/service/Printer.java
+++ week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/service/Printer.java
@@ -4,8 +4,9 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Printer {
+public class Printer implements Logger {
 
+  @Override
   public void log(String msg) {
     System.out.println(LocalDateTime.now() + " - my printer says: " + msg);
   }
