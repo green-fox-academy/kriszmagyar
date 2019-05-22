44527acaa9beb45c11790d2b05d370bd8e3d2cc9
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-22 15:05:04 +0200

    feat(students): add student list

--- week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/controller/StudentController.java
+++ week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.greenfoxacademy.dependecies.controller;
 
+import com.greenfoxacademy.dependecies.service.student.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/gfa")
 public class StudentController {
 
+  @Autowired
+  private StudentService studentService;
+
   @GetMapping
   public String index() {
     return "student/index";
+  }
+
+  @GetMapping("/list")
+  public String list(Model model) {
+    model.addAttribute("students", studentService.findAll());
+    return "student/list";
   }
 
 }
--- week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/service/student/StudentService.java
+++ week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/service/student/StudentService.java
@@ -1,19 +1,29 @@
 package com.greenfoxacademy.dependecies.service.student;
 
 import com.greenfoxacademy.dependecies.model.Student;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService implements Saver<Student> {
 
-  @Override
-  public List<Student> findAll() {
-    return null;
+  private List<Student> students;
+
+  public StudentService() {
+    students = new ArrayList<>();
+    students.add(new Student("Sanyi"));
+    students.add(new Student("Lilla"));
+    students.add(new Student("John"));
   }
 
   @Override
-  public void save(Student item) {
+  public List<Student> findAll() {
+    return students;
+  }
 
+  @Override
+  public void save(Student student) {
+    students.add(student);
   }
 }
