1098c64baf99038406fb662d1151e00852349b46
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-22 12:33:12 +0200

    feat(view): add templates

--- week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/controller/UsefulController.java
+++ week-07/day-03/dependecies/src/main/java/com/greenfoxacademy/dependecies/controller/UsefulController.java
@@ -1,15 +1,27 @@
 package com.greenfoxacademy.dependecies.controller;
 
+import com.greenfoxacademy.dependecies.service.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/useful")
 public class UsefulController {
 
-  @GetMapping("/useful")
+  @Autowired
+  private UtilityService utilityService;
+
+  @GetMapping
   public String index() {
     return "index";
+  }
+
+  @GetMapping("/colored")
+  public String getColoredPage() {
+    return "colored";
   }
 
 }
