d66476e26a723538ba67302a44e42723975564c4
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-23 10:11:09 +0200

    feat(login): add funcionality

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.foxclub.controller;
 
+import com.greenfox.foxclub.model.Fox;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,13 +15,14 @@ public class MainController {
   }
 
   @GetMapping("/login")
-  public String loginView() {
+  public String loginView(Model model) {
+    model.addAttribute("fox", new Fox());
     return "login";
   }
 
   @PostMapping("/login")
-  public String login() {
-    return "redirect:/";
+  public String login(Fox fox) {
+    return "redirect:/?name=" + fox.getName();
   }
 
 }
