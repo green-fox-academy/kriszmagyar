93f9f75b9c8f160b4ece9736ba548edd10183abe
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-23 10:05:42 +0200

    feat(login): add post

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -2,6 +2,7 @@ package com.greenfox.foxclub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -12,8 +13,13 @@ public class MainController {
   }
 
   @GetMapping("/login")
-  public String login() {
+  public String loginView() {
     return "login";
+  }
+
+  @PostMapping("/login")
+  public String login() {
+    return "redirect:/";
   }
 
 }
