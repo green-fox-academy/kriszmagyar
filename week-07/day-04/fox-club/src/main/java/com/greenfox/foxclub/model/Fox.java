e0b347d2224e91b721540dcc6264e42d72aa8241
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-23 10:59:26 +0200

    feat(Fox): add food and drink

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/Fox.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/Fox.java
@@ -1,14 +1,22 @@
 package com.greenfox.foxclub.model;
 
+import static com.greenfox.foxclub.model.Drink.COLA;
+import static com.greenfox.foxclub.model.Food.HAMBURGER;
+
 public class Fox {
 
   private String name;
+  private Food food;
+  private Drink drink;
+
+  public Fox() {
+    this("");
+  }
 
   public Fox(String name) {
     this.name = name;
-  }
-
-  public Fox() {
+    food = HAMBURGER;
+    drink = COLA;
   }
 
   public String getName() {
@@ -17,5 +25,21 @@ public class Fox {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public Food getFood() {
+    return food;
+  }
+
+  public void setFood(Food food) {
+    this.food = food;
+  }
+
+  public Drink getDrink() {
+    return drink;
+  }
+
+  public void setDrink(Drink drink) {
+    this.drink = drink;
   }
 }
