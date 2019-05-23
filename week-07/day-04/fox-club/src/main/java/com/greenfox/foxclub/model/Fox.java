c7819a9d36a90a20fcc39480c37970059701eaad
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-23 10:07:40 +0200

    feat(model): add Fox

---
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/Fox.java
@@ -0,0 +1,21 @@
+package com.greenfox.foxclub.model;
+
+public class Fox {
+
+  private String name;
+
+  public Fox(String name) {
+    this.name = name;
+  }
+
+  public Fox() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
