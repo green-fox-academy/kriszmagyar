d0be2fee3b726e24ba2881ddc940b0b7243b35e9
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-23 10:20:34 +0200

    feat(MainController): request param name checking

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/controller/MainController.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
   @GetMapping
-  public String index() {
+  public String index(@RequestParam(required = false) String name, Model model) {
+    if (name == null || name.isEmpty()) {
+      return "redirect:/login";
+    }
+
+    model.addAttribute("name", name);
     return "index";
   }
 
