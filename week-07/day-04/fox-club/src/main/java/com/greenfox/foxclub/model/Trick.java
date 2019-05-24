ddd22d18ce5c33ec4a8dcfeb359fdc80e9cc1da2
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-24 15:01:48 +0200

    feat(Trick): add some more

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/Trick.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/Trick.java
@@ -5,7 +5,10 @@ public enum Trick {
   CODE_IN_JAVA ("Code in Java"),
   WRITE_HTML ("Write HTML"),
   SUFFER_WITH_CSS ("Suffer with CSS"),
-  PLAY_GAMES ("Play games");
+  PLAY_GAMES ("Play games"),
+  WATCH_FIFTY_SHADES_OF_GRAY ("Watch fifty shades of gray"),
+  BE_COOL_AS_A_CUCUMBER ("Be cool as a cucumber"),
+  LAND_ON_MARS ("Land on mars");
 
   public final String name;
 
