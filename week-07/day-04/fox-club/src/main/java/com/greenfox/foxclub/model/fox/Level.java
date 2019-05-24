d6fdf8e39531ecd47a7ee2c03e389813b519d8c0
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-24 17:32:24 +0200

    feat(Level): init

--- week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/fox/Level.java
+++ week-07/day-04/fox-club/src/main/java/com/greenfox/foxclub/model/fox/Level.java
@@ -2,4 +2,25 @@ package com.greenfox.foxclub.model.fox;
 
 public class Level {
 
+  private int level;
+  private int currentXP;
+  private int requiredXP;
+
+  Level() {
+    level = 1;
+    currentXP = 0;
+    requiredXP = 10;
+  }
+
+  public int get() {
+    return level;
+  }
+
+  public int getCurrentXP() {
+    return currentXP;
+  }
+
+  public int getRequiredXP() {
+    return requiredXP;
+  }
 }
