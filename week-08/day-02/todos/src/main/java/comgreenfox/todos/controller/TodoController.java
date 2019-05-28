a892e7c02f181dd5a1fb805dc83271bf1565998d
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-28 10:54:12 +0200

    feat(service): add TodoService

--- week-08/day-02/todos/src/main/java/comgreenfox/todos/controller/TodoController.java
+++ week-08/day-02/todos/src/main/java/comgreenfox/todos/controller/TodoController.java
@@ -1,18 +1,22 @@
 package comgreenfox.todos.controller;
 
+import comgreenfox.todos.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
 
+  @Autowired
+  private TodoService todoService;
+
   @GetMapping(value = {"", "/list"})
   public String list(Model model) {
-    model.addAttribute("todos");
+    model.addAttribute("todos", todoService.findAll());
     return "todo-list";
   }
 
---
+++ week-08/day-02/todos/src/main/java/comgreenfox/todos/service/TodoServiceImp.java
@@ -0,0 +1,22 @@
+package comgreenfox.todos.service;
+
+import comgreenfox.todos.model.Todo;
+import comgreenfox.todos.repository.TodoRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoServiceImp implements TodoService {
+
+  @Autowired
+  private TodoRepository todoRepository;
+
+  @Override
+  public List<Todo> findAll() {
+    List<Todo> todos = new ArrayList<>();
+    todoRepository.findAll().forEach(todos::add);
+    return todos;
+  }
+}
