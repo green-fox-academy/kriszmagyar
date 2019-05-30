ed3acc28a408b6ab4d4a9187af49a5cc1e464d9e
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-30 12:49:53 +0200

    rafactor(todo): rename path todo -> todos

---
+++ week-08/day-02/todos/src/main/java/comgreenfox/todos/controller/AssigneeController.java
@@ -0,0 +1,10 @@
+package comgreenfox.todos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/assignees")
+public class AssigneeController {
+
+}
--- week-08/day-02/todos/src/main/java/comgreenfox/todos/controller/TodoController.java
+++ week-08/day-02/todos/src/main/java/comgreenfox/todos/controller/TodoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/todo")
+@RequestMapping("/todos")
 public class TodoController {
 
   @Autowired
@@ -20,43 +20,43 @@ public class TodoController {
   @GetMapping(value = {"", "/list"})
   public String list(String search, Model model) {
     model.addAttribute("todos", todoService.findWithQuery(search));
-    return "todo-list";
+    return "todo/todo-list";
   }
 
   @GetMapping("/{id}")
   public String itemView(@PathVariable long id, Model model) {
     model.addAttribute("todo", todoService.findById(id));
-    return "todo-item";
+    return "todo/todo-item";
   }
 
   @GetMapping("/add")
   public String addView(Model model) {
     model.addAttribute("todo", todoService.getNewInstance());
-    return "todo-add";
+    return "todo/todo-add";
   }
 
   @PostMapping("/add")
   public String add(Todo todo) {
     todoService.add(todo);
-    return "redirect:/todo";
+    return "redirect:/todos";
   }
 
   @GetMapping("/delete/{id}")
   public String delete(@PathVariable long id) {
     todoService.delete(id);
-    return "redirect:/todo";
+    return "redirect:/todos";
   }
 
   @GetMapping("/edit/{id}")
   public String editView(@PathVariable long id, Model model) {
     model.addAttribute("todo", todoService.findById(id));
-    return "todo-edit";
+    return "todo/todo-edit";
   }
 
   @PostMapping("/edit/{id}")
   public String edit(Todo todo) {
     todoService.edit(todo);
-    return "redirect:/todo";
+    return "redirect:/todos";
   }
 
 }
