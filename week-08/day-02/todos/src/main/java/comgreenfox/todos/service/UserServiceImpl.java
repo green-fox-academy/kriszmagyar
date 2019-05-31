6c7571153cc718801c520ec8afc08e21761f5ee7
Krisz Magyar <krisz.magyar@gmail.com>
2019-05-31 10:28:05 +0200

    feat(UserService): impl password validation

--- week-08/day-02/todos/src/main/java/comgreenfox/todos/service/UserServiceImpl.java
+++ week-08/day-02/todos/src/main/java/comgreenfox/todos/service/UserServiceImpl.java
@@ -24,15 +24,25 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public ValidationError getLoginValidationError(User user) {
+    ValidationError error = new ValidationError();
+    if (!isValidUsername(user.getName())) {
+      error.setUsername("This user is not exist!");
+      return error;
+    }
+    if (!isValidPassword(user)) {
+      error.setPassword("Password is incorrect!");
+      return error;
+    }
     return null;
   }
 
-  public boolean isValid(User user) {
+  private boolean isValidUsername(String username) {
+    return userRepository.findByName(username) != null;
+  }
+
+  private boolean isValidPassword(User user) {
     User userInRepo = userRepository.findByName(user.getName());
-    if (userInRepo == null) {
-      return false;
-    }
-    return !user.getPassword().equals(userInRepo.getPassword());
+    return userInRepo.getPassword().equals(user.getPassword());
   }
 
   @Override
