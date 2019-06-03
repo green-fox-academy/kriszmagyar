ba185133711871733ac2b9e38173bbe9fea088c6
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-03 11:58:23 +0200

    feat(greeter): init

---
+++ week-09/day-01/rest-exercie/src/main/java/com/greenfox/restexercie/model/Greeter.java
@@ -0,0 +1,23 @@
+package com.greenfox.restexercie.model;
+
+public class Greeter {
+
+  private String name;
+  private String title;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+}
