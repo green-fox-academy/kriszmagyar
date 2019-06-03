2e17e315556e4a337d1b181c3efa769be32f3433
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-03 10:56:57 +0200

    feat(rest-exercise): init

---
+++ week-09/day-01/rest-exercie/src/main/java/com/greenfox/restexercie/RestExercieApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.restexercie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExercieApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestExercieApplication.class, args);
+  }
+
+}
