377a254e1a0587cf93da87d8384539f9960a737d
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-04 09:02:11 +0200

    feat(guardians-of-the-galaxy): init

---
+++ week-09/day-02/guardians-of-the-galaxy/src/main/java/com/greenfox/guardiansofthegalaxy/GuardiansOfTheGalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.guardiansofthegalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardiansOfTheGalaxyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuardiansOfTheGalaxyApplication.class, args);
+  }
+
+}
---
+++ week-09/day-02/guardians-of-the-galaxy/src/test/java/com/greenfox/guardiansofthegalaxy/GuardiansOfTheGalaxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.guardiansofthegalaxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuardiansOfTheGalaxyApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
