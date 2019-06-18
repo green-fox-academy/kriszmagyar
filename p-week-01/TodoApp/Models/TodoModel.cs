12e2d7a89adb56a44fd5e72813bf2c9018871805
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-18 09:15:05 +0200

    feat(TodoApp): init

---
+++ p-week-01/TodoApp/Models/TodoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class TodoModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+    }
+}
