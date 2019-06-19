ce7a748c3863688ac1da29b868b8c9deade555d7
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-19 14:32:38 +0200

    feat(TodoController): add Authorize

--- p-week-01/TodoApp/Controllers/TodoController.cs
+++ p-week-01/TodoApp/Controllers/TodoController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TodoApp.Models;
 using TodoApp.Services;
 
 namespace TodoApp.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class TodoController : ControllerBase
