143e3c861061a1528567cf174453bc917c14fbcb
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-20 15:23:06 +0200

    feat(UserModel): add fields for password hashing

--- p-week-01/TodoApp/Models/UserModel.cs
+++ p-week-01/TodoApp/Models/UserModel.cs
@@ -13,6 +13,8 @@ namespace TodoApp.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
         public string Role { get; set; }
         public string Token { get; set; }
     }
--- p-week-01/TodoApp/Services/AuthService.cs
+++ p-week-01/TodoApp/Services/AuthService.cs
@@ -36,11 +36,31 @@ namespace TodoApp.Services
         private UserModel GetValidUser(string username, string password)
         {
             var user = userService.FindByUsername(username);
-            if (user == null || !user.Password.Equals(password))
+            if (user == null || VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
             {
-                return null;
+                throw new Exception($"User is not verified, username: {username}," +
+                    $" password: {password}, hash: {user.PasswordHash}, salt: {user.PasswordSalt}");
             }
             return user;
+        }
+
+        private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
+        {
+            if (password == null) throw new ArgumentNullException("password");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
+
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != storedHash[i]) return false;
+                }
+            }
+
+            return true;
         }
 
         private string GetUserToken(UserModel user)
