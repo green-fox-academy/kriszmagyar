ea97a6b9be119178a21e2acff974fdd17c2d123d
Krisz Magyar <krisz.magyar@gmail.com>
2019-06-21 09:19:47 +0200

    refactor(Role): move to User

--- p-week-01/TodoApp/Models/User/Role.cs
+++ p-week-01/TodoApp/Models/User/Role.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TodoApp.Models
+namespace TodoApp.Models.User
 {
     public static class Role
     {
